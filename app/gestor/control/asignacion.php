2c9ee8e40e67d77dd186ba1ee0bc961ec261b831
melqui romero <melquiromero@iMac-de-melqui.local>
2025-04-03 14:21:39 -0500

    estilos ajuste en archivos

--- app/gestor/control/asignacion.php
+++ app/gestor/control/asignacion.php
@@ -35,7 +35,7 @@ $detalleCRP = $miClaseG->obtenerDetalleCRP($detalleSaldo['CODIGO_CRP'], '*'); //
     <title>Detalle del Registro</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <link rel="icon" href="<?php echo BASE_URL; ?>assets/img/public/logosena.png">
-    <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/presupuesto/index_gestor.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/gestor/index_gestor.css">
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/gestor/asignacion.css">
 </head>
 <body>
--- app/presupuesto/index.php
+++ app/presupuesto/index.php
@@ -29,7 +29,7 @@ $initialData = $miClaseG->obtenerCDP($numeroDocumento, $fuente, $reintegros, 10,
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <link rel="icon" href="<?php echo BASE_URL; ?>assets/img/public/logosena.png">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/presupuesto/index_gestor.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/presupuesto/index_presupuesto.css">
 </head>
 <body>
     <?php require_once $_SERVER['DOCUMENT_ROOT'] . '/public/share/nav.php'; ?>
