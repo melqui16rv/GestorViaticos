57935b8dcf2d015c67f94668d3770620b91cbb04
melqui romero <melquiromero@iMac---melqui.local>
2025-05-02 14:14:36 -0500

    nueva dependencia, 62

--- math/general_sennova/graficas.php
+++ math/general_sennova/graficas.php
@@ -30,6 +30,7 @@ class graficas_general_sennova extends Conexion{
         '43' => 'Bienestar Empleados',
         '44' => 'Apoyos Sostenible',
         '45' => 'Regular',
+        '62' => 'WORLDSKILLS',
         '69' => 'Tecnoparque',
         '70' => 'Tecnoacademia',
         '84' => 'ECCL Campesena',
