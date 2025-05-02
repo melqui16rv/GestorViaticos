601fb3503902099fd5fed488500da168066bf797
melqui romero <melquiromero@iMac---melqui.local>
2025-05-02 14:02:08 -0500

    metodos ajustados en ruta 13

--- app/SENNOVA/General/dashboard_content.php
+++ app/SENNOVA/General/dashboard_content.php
@@ -1,7 +1,8 @@
 <?php
-ni_set('display_errors', 1);
+ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+
 require_once $_SERVER['DOCUMENT_ROOT'] . '/conf/config.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/math/general_sennova/user.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/math/general_sennova/graficas.php';
