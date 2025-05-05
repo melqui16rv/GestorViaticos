e339dd766106c42aa46e9d544a749708cba3f83f
melqui romero <melquiromero@iMac---melqui.local>
2025-05-05 15:55:49 -0500

    prueba de estilos automaticos

--- includes/session/login.php
+++ includes/session/login.php
@@ -37,7 +37,7 @@ if (isset($_POST['Validar'])) {
     <meta charset="UTF-8">
     <title>Inicio de Sesión SENA</title>
     <link rel="icon" href="<?php echo BASE_URL; ?>assets/img/public/logosena.png">
-    <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/share/login.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/share/login.css?v=<?php echo filemtime($_SERVER['DOCUMENT_ROOT'] . '/assets/css/share/login.css'); ?>">
     <link rel="preload" as="image" href="<?php echo BASE_URL; ?>assets/img/public/foto.jpeg">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <style>
