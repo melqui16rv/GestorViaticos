8941ba465941ea188cd16b8a273ccb7c9947a14c
melqui romero <melquiromero@iMac---melqui.local>
2025-05-12 11:30:21 -0500

    css tabla 3

--- app/SENNOVA/Tecnoparque/metas/VisitasApre.php
+++ app/SENNOVA/Tecnoparque/metas/VisitasApre.php
@@ -6,12 +6,17 @@ $metas = new metas_tecnoparque();
 
 // Agregar esta función auxiliar después de los requires
 function formatearFecha($fecha) {
-    setlocale(LC_TIME, 'es_ES.UTF-8', 'Spanish_Spain.1252');
+    $meses = [
+        1 => 'enero', 2 => 'febrero', 3 => 'marzo', 4 => 'abril',
+        5 => 'mayo', 6 => 'junio', 7 => 'julio', 8 => 'agosto',
+        9 => 'septiembre', 10 => 'octubre', 11 => 'noviembre', 12 => 'diciembre'
+    ];
+    
     $timestamp = strtotime($fecha);
     $dia = date('j', $timestamp);
-    $mes = strftime('%B', $timestamp);
+    $mes = $meses[date('n', $timestamp)];
     $anio = date('Y', $timestamp);
-    $hora = date('g:i a', $timestamp);
+    $hora = strtolower(date('g:i a', $timestamp));
     
     return $dia . ' de ' . $mes . ' ' . $anio . '<br>' . $hora;
 }
