ab02c87da8ab51034924f84924708a139830d1fb
melqui romero <melquiromero@iMac---melqui.local>
2025-07-29 15:44:12 -0500

    despliegue main 3

--- conf/auth.php
+++ conf/auth.php
@@ -1,6 +1,6 @@
 <?php
-// Incluir config.php antes de cualquier otro código
-require_once '/Users/melquiromero/Documents/GitHub/viaticosApp/conf/auth.php';
+// Incluir config.php antes de cualquier otro códig
+require_once '/home/appscide/public_html/viaticosApp/conf/auth.php';
 
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
