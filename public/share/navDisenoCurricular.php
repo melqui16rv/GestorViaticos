9960142b199d3324bbb9683c491024895d8fe703
melqui16rv <melquiveru@gmail.com>
2025-07-30 10:53:22 -0500

    mdmd

--- public/share/navDisenoCurricular.php
+++ public/share/navDisenoCurricular.php
@@ -4,8 +4,8 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 ob_start();
-require_once $_SERVER['DOCUMENT_ROOT'] . '/conf/config.php';
-require_once '/home/appscide/public_html/viaticosApp/math/gen/user.php';
+require_once dirname(__DIR__, 2) . '/conf/config.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/math/gen/user.php';
 
 
 if (isset($_GET['num_doc'])) {
@@ -38,8 +38,8 @@ if (isset($_SESSION['numero_documento'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <meta name="theme-color" content="#2c3e50">
-    <link rel="icon" href="/home/appscide/public_html/viaticosApp/assets/img/public/logosena.png">
-    <link rel="stylesheet" href="/home/appscide/public_html/viaticosApp/assets/css/share/nav.css">
+    <link rel="icon" href="<?php echo BASE_URL; ?>assets/img/public/logosena.png">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>assets/css/share/nav.css">
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
     <title>Viaticos</title>
 </head>
@@ -63,12 +63,12 @@ if (isset($_SESSION['numero_documento'])) {
         <div class="navbar-left">
             <!-- Botón de sesión prioritario en móvil -->
             <div class="session-button-mobile">                <?php if(isset($_SESSION['id_rol'])):?>
-                    <a href="/home/appscide/public_html/viaticosApp/includes/session/salir.php" class="boton_ir mobile-session">
+                    <a href="<?php echo BASE_URL; ?>includes/session/salir.php" class="boton_ir mobile-session">
                         <i class="fas fa-sign-out-alt"></i>
                         <span>Salir</span>
                     </a>
                 <?php else: ?>
-                    <a href="/home/appscide/public_html/viaticosApp/includes/session/login.php" class="boton_ir mobile-session">
+                    <a href="<?php echo BASE_URL; ?>includes/session/login.php" class="boton_ir mobile-session">
                         <i class="fas fa-sign-in-alt"></i>
                         <span>Entrar</span>
                     </a>
@@ -126,7 +126,7 @@ if (isset($_SESSION['numero_documento'])) {
                             <?php
                                 $currentPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);                        
                                 ?>
-                            <a href="/home/appscide/public_html/viaticosApp/public/share/dashboard.php" 
+                            <a href="<?php echo BASE_URL; ?>public/share/dashboard.php" 
                             class="<?php echo ($currentPath === '/viaticosApp/public/share/dashboard.php') ? 'activeURL' : ''; ?>">Panel Datos</a>
                         </li>
                         <?php endif; ?>
@@ -136,35 +136,7 @@ if (isset($_SESSION['numero_documento'])) {
                         <?php if (isset($_SESSION['id_rol']) && $_SESSION['id_rol'] == '7'): ?>
                             <!-- Bloque exclusivo para usuarios con rol 4 (SENNOVA) -->
                             <li>
-                                <a href="/home/appscide/public_html/disenoCurricular/index.php"
-                                    class="<?php echo ('/home/appscide/public_html/disenoCurricular/index.php') ? 'activeURL' : ''; ?>">
-                                    Diseno Curricular
-                                </a>
-                            </li>
-                        <!-- Puedes agregar más opciones aquí para el rol 4 -->
-                        <?php endif; ?>
-                        <?php if (isset($_SESSION['id_rol']) && $_SESSION['id_rol'] == '4'): ?>
-                            <!-- Bloque exclusivo para usuarios con rol 4 (SENNOVA) -->
-                            <li>
-                                <a href="/home/appscide/public_html/viaticosApp/app/SENNOVA/General/index.php"
-                                    class="<?php echo ($currentPath === '/viaticosapp/app/sennova/general/index.php') ? 'activeURL' : ''; ?>">
-                                    Presupuesto - SENNOVA
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/home/appscide/public_html/viaticosApp/app/SENNOVA/Tecnoparque/metas/index.php"
-                                    class="<?php echo ($currentPath === '/viaticosapp/app/sennova/tecnoparque/metas/index.php') ? 'activeURL' : ''; ?>">
-                                    Metas - Tecnoparque
-                                </a>
-                            </li>
-                        <!-- Puedes agregar más opciones aquí para el rol 4 -->
-                        <?php endif; ?>
-                        <!-- ----- fin para rol 4--------- -->
-                        <!-- ----- inicio para rol 9--------- --------------->
-                        <?php if (isset($_SESSION['id_rol']) && $_SESSION['id_rol'] == '7'): ?>
-                            <!-- Bloque exclusivo para usuarios con rol 9 (SENNOVA) -->
-                            <li>
-                                <a href="/home/appscide/public_html/viaticosApp/app/SENNOVA/General/index.php"
+                                <a href="<?php echo BASE_URL; ?>app/SENNOVA/General/index.php"
                                     class="<?php echo ($currentPath === '/viaticosapp/app/sennova/general/index.php') ? 'activeURL' : ''; ?>">
                                     Solicitud Rol
                                 </a>
@@ -172,21 +144,34 @@ if (isset($_SESSION['numero_documento'])) {
                         <!-- Puedes agregar más opciones aquí para el rol 4 -->
                         <?php endif; ?>
                         <?php if (isset($_SESSION['id_rol']) && $_SESSION['id_rol'] == '4'): ?>
+                            <!-- Bloque exclusivo para usuarios con rol 4 (SENNOVA) -->
+                            <li>
+                                <a href="<?php echo BASE_URL; ?>app/SENNOVA/General/index.php"
+                                    class="<?php echo ($currentPath === '/viaticosapp/app/sennova/general/index.php') ? 'activeURL' : ''; ?>">
+                                    Presupuesto - SENNOVA
+                                </a>
+                            </li>
+                            <li>
+                                <a href="<?php echo BASE_URL; ?>app/SENNOVA/Tecnoparque/metas/index.php"
+                                    class="<?php echo ($currentPath === '/viaticosapp/app/sennova/tecnoparque/metas/index.php') ? 'activeURL' : ''; ?>">
+                                    Metas - Tecnoparque
+                                </a>
+                            </li>
                         <!-- Puedes agregar más opciones aquí para el rol 4 -->
                         <?php endif; ?>
-                        <!-- ----- fin para rol 9------------------------ -->
+                        <!-- ----- fin para rol 4--------- -->
                         <!-- ----- inicio para rol 5--------- -->
                         
                         <?php if (isset($_SESSION['id_rol']) && $_SESSION['id_rol'] == '5'): ?>
                             <!-- Bloque exclusivo para usuarios con rol 5 (Tecnoparque) -->
                             <li>
-                                <a href="/home/appscide/public_html/viaticosApp/app/SENNOVA/General/index.php"
+                                <a href="<?php echo BASE_URL; ?>app/SENNOVA/General/index.php"
                                     class="<?php echo ($currentPath === '/viaticosapp/app/sennova/general/index.php') ? 'activeURL' : ''; ?>">
                                     Presupuesto
                                 </a>
                             </li>
                             <li>
-                                <a href="/home/appscide/public_html/viaticosApp/app/SENNOVA/Tecnoparque/metas/index.php"
+                                <a href="<?php echo BASE_URL; ?>app/SENNOVA/Tecnoparque/metas/index.php"
                                     class="<?php echo ($currentPath === '/viaticosapp/app/sennova/tecnoparque/metas/index.php') ? 'activeURL' : ''; ?>">
                                     Metas
                                 </a>
@@ -199,7 +184,7 @@ if (isset($_SESSION['numero_documento'])) {
                         <?php if (isset($_SESSION['id_rol']) && $_SESSION['id_rol'] == '6'): ?>
                             <!-- Bloque exclusivo para usuarios con rol 6 (Tecnoacademia) -->
                             <li>
-                                <a href="/home/appscide/public_html/viaticosApp/app/SENNOVA/General/index.php"
+                                <a href="<?php echo BASE_URL; ?>app/SENNOVA/General/index.php"
                                     class="<?php echo ($currentPath === '/viaticosapp/app/sennova/general/index.php') ? 'activeURL' : ''; ?>">
                                     Presupuesto
                                 </a>
@@ -214,7 +199,7 @@ if (isset($_SESSION['numero_documento'])) {
                             $currentPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
                             ?>
                             
-                            <a href="/home/appscide/public_html/viaticosApp/app/presupuesto/index.php" 
+                            <a href="<?php echo BASE_URL; ?>app/presupuesto/index.php" 
                             class="<?php echo ($currentPath === '/viaticosApp/app/presupuesto/index.php') ? 'activeURL' : ''; ?>">Registros RP</a>
                             
                         </li>
@@ -223,7 +208,7 @@ if (isset($_SESSION['numero_documento'])) {
                             $currentPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
                             ?>
                             
-                            <a href="/home/appscide/public_html/viaticosApp/app/presupuesto/historialOP.php" 
+                            <a href="<?php echo BASE_URL; ?>app/presupuesto/historialOP.php" 
                             class="<?php echo ($currentPath === '/viaticosApp/app/presupuesto/historialOP.php') ? 'activeURL' : ''; ?>">Registros RP (Viáticos)</a>
                         </li>
                 <!-- ----- fin para rol 3--------- -->
@@ -234,7 +219,7 @@ if (isset($_SESSION['numero_documento'])) {
                             $currentPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
                             ?>
                             
-                            <a href="/home/appscide/public_html/viaticosApp/app/admin/index.php" 
+                            <a href="<?php echo BASE_URL; ?>app/admin/index.php" 
                             class="<?php echo ($currentPath === '/viaticosApp/app/admin/index.php') ? 'activeURL' : ''; ?>">Panel de control</a>
                         </li>
                         <li>
@@ -242,7 +227,7 @@ if (isset($_SESSION['numero_documento'])) {
                             $currentPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
                             ?>
                             
-                            <a href="/home/appscide/public_html/viaticosApp/app/admin/solicitudes/index.php" 
+                            <a href="<?php echo BASE_URL; ?>app/admin/solicitudes/index.php" 
                             class="<?php echo ($currentPath === '/viaticosApp/app/admin/solicitudes') ? 'activeURL' : ''; ?>">Solicitudes de cambio de rol</a>
                         </li>
                 <!-- ----- fin para rol 1--------- -->
@@ -253,7 +238,7 @@ if (isset($_SESSION['numero_documento'])) {
                             $currentPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
                             ?>
                             
-                            <a href="/home/appscide/public_html/viaticosApp/app/gestor/index.php" 
+                            <a href="<?php echo BASE_URL; ?>app/gestor/index.php" 
                                class="<?php echo ($currentPath === '/viaticosApp/app/gestor/index.php') ? 'activeURL' : ''; ?>">
                                Gestor
                             </a>
@@ -262,7 +247,7 @@ if (isset($_SESSION['numero_documento'])) {
                   <!-- ----- Enlace de perfil para todos los usuarios logueados--------- -->
                 <!-- <?php //if (isset($_SESSION['numero_documento'])): ?>
                     <li>
-                  
+                        <a href="<?php //echo BASE_URL; ?>public/share/cuenta.php" 
                            class="<?php //echo ($currentPath === '/viaticosapp/public/share/cuenta.php') ? 'activeURL' : ''; ?>">
                            <i class="fas fa-user-circle" style="margin-right: 8px;"></i>Mi Perfil
                         </a>
@@ -283,14 +268,14 @@ if (isset($_SESSION['numero_documento'])) {
               <!-- Botón de sesión para desktop -->
             <div class="session-button-desktop">
                 <?php if(isset($_SESSION['id_rol'])):?>
-                    <a href="/home/appscide/public_html/viaticosApp/includes/session/salir.php" class="boton_ir">Cerrar sesión</a>
+                    <a href="<?php echo BASE_URL; ?>includes/session/salir.php" class="boton_ir">Cerrar sesión</a>
                 <?php else: ?>
-                    <a href="/home/appscide/public_html/viaticosApp/includes/session/login.php" class="boton_ir">Iniciar sesión</a>
+                    <a href="<?php echo BASE_URL; ?>includes/session/login.php" class="boton_ir">Iniciar sesión</a>
                 <?php endif; ?>
             </div>
         </div>
     </div>
-    <script src="/home/appscide/public_html/viaticosApp/assets/js/header.js"></script>
+    <script src="<?php echo BASE_URL; ?>assets/js/header.js"></script>
 </body>
 </html>
 <script>
@@ -305,7 +290,7 @@ function salir() {
         confirmBtn.onclick = () => {
             modal.classList.remove('active');
             resolve(true);
-            window.location.href = '/home/appscide/public_html/viaticosApp/includes/session/salir.php';
+            window.location.href = '<?php echo BASE_URL; ?>includes/session/salir.php';
         };
         
         cancelBtn.onclick = () => {
